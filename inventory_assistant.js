52ea568cf641d432a6204c8ce354d4c7fcc08cd9
Ariella <ariellayavno@usf.edu>
2025-09-06 22:58:24 -0400

    edited code

---
+++ inventory_assistant.js
@@ -0,0 +1,25 @@
+// Coding Challenge 00
+
+// Code goes here
+
+const itemName = football
+
+let unitCost = 5.00
+let currentStock = 200
+let reorderLevel = 50
+let targetSStock = 400
+let weeklyDemand = 35
+let supplierLeadTimeWeeks = 1
+
+let weeksOfCover = weeklyDemand > 0 ? currentStock / weeklyDemand : Infinity
+let stockDeficit = Math.max(0, targetStock - currentStock)
+let reorderQuantity = (currentStock <= reorderLevel || weeksOfCover <
+   supplierLeadTimeWeeks) ? Math.ceil(stockDeficit) : 0
+let estimatedReorderCost = reorderQuantity * unitCost
+let reorderNow = currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks
+
+console.log(itemName)
+console.log(weeksOfCover.toFixed(2))
+console.log(reorderNow)
+console.log(reorderQuantity)
+console.log(estimatedReorderCost.toFixed(2))
